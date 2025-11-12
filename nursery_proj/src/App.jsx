9a270a1df51abb4513337db9fe83c6870c809559
mayurchavda0711 <mayurr0711@gmail.com>
2025-11-12 12:46:47 +0530

    update

--- nursery_proj/src/App.jsx
+++ nursery_proj/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -8,8 +9,9 @@ function App() {
       <Header />
       <div className="container mt-5 text-center text-success">
         {/* <h2>Welcome to Shree Ram Dharu 🌿</h2> */}
-        <Footer />
+        
       </div>
+      <Footer />
     </>
   );
 }
--- nursery_proj/src/components/layout/Footer.jsx
+++ nursery_proj/src/components/layout/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Footer.css";
 import { Leaf } from "lucide-react";
 
 const Footer = () => {
