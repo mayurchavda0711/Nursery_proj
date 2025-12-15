91cad6ac2c0c7be39db791385b1ce01d66dd15b0
mayurchavda0711 <mayurr0711@gmail.com>
2025-12-15 19:53:07 +0530

    u

--- nursery_proj/src/main.jsx
+++ nursery_proj/src/main.jsx
@@ -5,18 +5,18 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css';
 import App from './App.jsx';
 
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { CartProvider } from "./context/CartContext";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <CartProvider>
           <App />
         </CartProvider>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
